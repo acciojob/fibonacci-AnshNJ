c0bcfa87da51fa9557943503c2634f8975f628d3
Ansh Jain <anshjain866@gmail.com>
2024-03-01 20:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-// your code here
+	let n1=0;
+	let n2=1;
+	if(num == 0) return n1;
+	if(num == 1) return n2;
+	let n3;
+	for(int i=2; i<n; i++){
+		n3 = n1+n2;
+		n1 = n2;
+		n2 = n3;
+	}
+	return n2;
 }
-
 module.exports = fibonacci;
